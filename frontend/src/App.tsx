e209fe4f746a9d170dcab236892893a269a81632
yuanyoung <997816500@qq.com>
2021-11-28 01:10:01 +0000

    !3 chore: 修改界面布局 * chore: 修改界面布局

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Canvas from '@components/Canvas';
 import AttrubutionBox from '@components/AttributionBox';
 import CommandLine from '@components/CommandLine';
 import 'antd/dist/antd.css';
-import {Simulate} from "react-dom/test-utils";
+import { Simulate } from "react-dom/test-utils";
 import select = Simulate.select;
 
 const { Sider, Content } = Layout;
@@ -18,23 +18,23 @@ function App() {
     fresh: false
   });
 
-  const topologies=[
+  const topologies = [
     {
-      id:1,
-      name:"拓扑一"
+      id: 1,
+      name: "拓扑一"
     },
     {
-      id:2,
-      name:"拓扑二"
+      id: 2,
+      name: "拓扑二"
     },
     {
-      id:3,
-      name:"拓扑三"
+      id: 3,
+      name: "拓扑三"
     }
-    ];
-  const selectTopology=(e:any)=>{
-      const topologyId=e.key;
-      //todo,刷新当前的拓扑项目
+  ];
+  const selectTopology = (e: any) => {
+    const topologyId = e.key;
+    //todo,刷新当前的拓扑项目
   };
 
   return (
@@ -45,26 +45,26 @@ function App() {
         fresh: state.fresh,
         setDeviceId: (deviceId) => setState(obj => ({ ...obj, deviceId })),
         setTopologyId: (topologyId) => setState(obj => ({ ...obj, topologyId })),
-        setFresh: () => setState(o => ({...o, fresh: !o.fresh}))
+        setFresh: () => setState(o => ({ ...o, fresh: !o.fresh }))
       }}
     >
       <Layout style={{ height: '100vh' }}>
         <Sider width={200} collapsible collapsedWidth={0} zeroWidthTriggerStyle={{ top: 0 }} >
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} onClick={selectTopology}>
-            {topologies.map((value)=>{
+            {topologies.map((value) => {
               return <Menu.Item key={value.id} >
-                  {value.name}
+                {value.name}
               </Menu.Item>
             })}
           </Menu>
         </Sider>
-        <Layout style={{ height: '100%' }}>
-          <Layout style={{ display: 'flex', flexDirection: 'row', height: '60%' }}>
-            <Content style={{ width: '30vw' }}><Canvas /></Content>
-            <Content><AttrubutionBox /></Content>
+        <Layout style={{display: 'flex', flexDirection: 'row'}}>
+          <Layout style={{width: '40vw'}}>
+            <Content><Canvas /></Content>
           </Layout>
-          <Layout style={{ height: '40%' }}>
-            <Content><CommandLine /></Content>
+          <Layout style={{height: '100vh'}}>
+            <Content style={{height: '50vh'}}><AttrubutionBox /></Content>
+            <Content style={{height: '50vh'}}><CommandLine /></Content>
           </Layout>
         </Layout>
       </Layout>
